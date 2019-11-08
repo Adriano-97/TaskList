2365ad502c188887de564a35971035f0cd7d2b34
Adriano-97 <adriano199726@gmail.com>
2019-11-08 16:31:24 -0500

    Added the Basic funtionality for the the TodoList app

--- src/App.js
+++ src/App.js
@@ -4,11 +4,41 @@ import './components/Todos';
 import Todos from './components/Todos';
 
 class App extends Component {
+  //Toggle button function
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'take out trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Game',
+        completed: false
+      }
+    ]
+  };
   render() {
     return (
       <div className='App'>
         <h1>App</h1>
-        <Todos />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
