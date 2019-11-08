370836edf08b07edd9799c65b509874174a065f9
Adriano-97 <adriano199726@gmail.com>
2019-11-08 16:32:56 -0500

    This class Will invoke and organize all the to do items.

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
   render() {
-    return (
-      <div className='Todos'>
-        <h1>todos</h1>
-      </div>
-    );
+    return this.props.todos.map(todo => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+      />
+    ));
   }
 }
 
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+};
 export default Todos;
