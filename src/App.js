30a62d2888a59aa7313ed3cbb345e91a245fe8e7
Adriano-97 <adriano199726@gmail.com>
2019-11-12 18:06:25 -0500

    Added a header an an add to do Component.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import './components/Todos';
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 
 class App extends Component {
-  //Toggle button function
-  markComplete = id => {
-    this.setState({
-      todos: this.state.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      })
-    });
-  };
   state = {
     todos: [
       {
@@ -34,11 +25,37 @@ class App extends Component {
       }
     ]
   };
+
+  //Toggle button function
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+  //Delete Todo Item
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+
   render() {
     return (
       <div className='App'>
-        <h1>App</h1>
-        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+        <Header />
+        <div className='container'>
+          <AddTodo />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
